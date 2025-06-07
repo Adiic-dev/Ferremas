8278f4014e0748743fd7998432e21c76f01b2b2d
WhiteSouls20 <mat.orostica@duocuc.cl>
2025-06-07 09:35:45 -0400

    asdfsadf

---
+++ frontend/src/app/asdf/asdf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-asdf',
+  imports: [],
+  templateUrl: './asdf.component.html',
+  styleUrl: './asdf.component.scss'
+})
+export class AsdfComponent {
+
+}
